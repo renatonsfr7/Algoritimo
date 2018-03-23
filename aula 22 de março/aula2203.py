ff7c99baeb04563f3a62c3f5796e7081999d6968
Renato Nascimento Ferreira <renatonsfr7@gmail.com>
2018-03-22 22:00:12 -0300

    Criação das atividades

---
+++ aula 22 de março/aula2203.py
@@ -0,0 +1,55 @@
+# valor = int(input("digite um valor:"))
+#
+# if valor >0:
+#      print("Positivo")
+#
+# else:
+#     print("Negativo")
+
+#2-Faça um Programa que peça dois números e imprima o maior deles.
+# a = int(input("Digite um números:"))
+# b = int(input("Digite outro número:"))
+# if a >b:
+#     print("Este é o maior:", a)
+#
+# else:
+#     print("Este é maior:", b)
+
+#3-Faça um algoritmo que leia os valores A, B, C e imprima na tela se a soma de A + B é menor que C.
+# a = int(input("A:"))
+# b = int(input('B:'))
+# c = int(input("C:"))
+#
+# valor = a + b
+#
+# if valor <c:
+#     print("Este valor é menor que C:")
+# else:
+#     print("Este valor é maior que C:",)
+
+#4-Faça um Programa que verifique o sexo de uma pessoa. O usuário deve informar ‘F’ ou ‘M’ e o programa deve escrever “Feminino” ou “Masculino”, conforme a letra digitada.
+# sexo = input("Digite o seu sexo:")
+#
+# if sexo == "m" :
+#     print("Maculino")
+#
+# else:
+#     sexo == "f"
+#     print("Feminino")
+
+#5-Faça um Programa que peça um número inteiro e determine se ele é par ou ímpar. Dica: utilize o operador módulo (resto da divisão).
+# valor = int(input("Digite um número inteiro:"))
+#
+# if valor %2==0:
+#     print("Par")
+# else:
+#     print("Impar")
+
+#6-Faça um algoritmo que peça um número e se ele for par some 5, se não, some 8.
+valor = int(input("Digite um número:"))
+
+if valor %2==0:
+    print(valor + 5)
+
+else:
+    print(valor + 8)
